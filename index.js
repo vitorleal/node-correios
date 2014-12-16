18804848394163ba98fd550ea53cf90d8f8ae6ea
Vitor Leal <vitorleal1@gmail.com>
2014-12-16 12:14:53 -0200

    fix indexjs

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-//module.exports = require('./lib/correios');
-var Correios = require('./lib/correios.js'),
-    correios = new Correios();
+module.exports = require('./lib/correios');
 
-//executa o método de pesquisa de valor do frete
-correios.consultaCEP({ cep: '04569001' });
-
-//quando o evento result for emitido faz um log do retorno da api
-correios.on('result', function (result) {
-  console.log(result);
-});
-
-//se ocorreu algum erro na execução faz um log do erro
-correios.on('error', function (err) {
-  console.log(err);
-});
