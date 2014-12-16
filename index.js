7eaeb272441b4267047a54f1a54c33974865c116
Vitor Leal <vitorleal1@gmail.com>
2014-12-16 12:02:25 -0200

    add address searhc by CEP, fix #2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/correios');
+//module.exports = require('./lib/correios');
+var Correios = require('./lib/correios.js'),
+    correios = new Correios();
+
+//executa o método de pesquisa de valor do frete
+correios.consultaCEP({ cep: '04569001' });
+
+//quando o evento result for emitido faz um log do retorno da api
+correios.on('result', function (result) {
+  console.log(result);
+});
+
+//se ocorreu algum erro na execução faz um log do erro
+correios.on('error', function (err) {
+  console.log(err);
+});
