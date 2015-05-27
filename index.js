1f614a9b4f1c671db274c05167dca50cbf70fbb1
Vitor Leal <vitorleal1@gmail.com>
2015-05-27 17:47:33 -0300

    fix export module

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-//module.exports = require('./lib/correios');
-var Correios = require('./lib/correios'),
-    correios = new Correios();
-
-correios.consultaCEP({ 'cep': '0456901' });
-
-correios.on('result', function (result) {
-  console.log(result);
-});
-
-correios.on('error', function (err) {
-  console.log(err);
-});
+module.exports = require('./lib/correios');
