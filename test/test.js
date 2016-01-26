324767352653049b7bc9c03379d66e5d45825c22
Luan Soares <soares.luan92@gmail.com>
2016-01-26 14:06:36 -0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -41,10 +41,12 @@ describe('Correios class', function() {
 });
 
 describe('Correios prototype', function() {
+  it('should have validArg', function() {
+    correios.should.have.property('validArg').which.is.a.Function;
+  });
   it('should have calcPreco', function() {
     correios.should.have.property('calcPreco').which.is.a.Function;
   });
-
   it('should have calcPrecoPrazo', function() {
     correios.should.have.property('calcPrecoPrazo').which.is.a.Function;
   });
@@ -63,5 +65,75 @@ describe('Correios prototype', function() {
     (function() {
       correios.consultaCEP({ cep: '00000000' });
     }).should.not.throw();
+  });
+
+  it('should throw exception if some of require fields is not passed in the arguments {ex: missing nCdServico }', function() {
+    (function() {
+      var args = {
+        sCepOrigem: '31275000',
+        sCepDestino:'32400000',
+        nVlPeso :'0.300',
+        nCdFormato :'3',
+        nVlComprimento : 30.0,
+        nVlAltura : 3,
+        nVlLargura : 30.0,
+        nVlDiametro :30.0,
+        zeta:123
+      }
+      correios.calcPrecoPrazo(args);
+    }).should.throw();
+  });
+
+  it('should not throw exception if all fields is passed in the arguments and the type is right', function() {
+    (function() {
+      var args = {
+        nCdServico: '40010,41106,40215',
+        sCepOrigem: '31275000',
+        sCepDestino:'32400000',
+        nVlPeso :'0.300',
+        nCdFormato :3,
+        nVlComprimento : 30.0,
+        nVlAltura : 3,
+        nVlLargura : 30.0,
+        nVlDiametro :30.0,
+        zeta:123
+      }
+      correios.calcPrecoPrazo(args);
+    }).should.not.throw();
+  });
+
+   it('should throw exception if nCdFormato is setted as 3 and nVlPeso is greater then 1', function() {
+    (function() {
+      var args = {
+        nCdServico: '40010,41106,40215',
+        sCepOrigem: '31275000',
+        sCepDestino:'32400000',
+        nVlPeso :'2.300',
+        nCdFormato :3,
+        nVlComprimento : 30.0,
+        nVlAltura : 3,
+        nVlLargura : 30.0,
+        nVlDiametro :30.0,
+        zeta:123
+      }
+      correios.calcPrecoPrazo(args);
+    }).should.throw();
+  });
+  it('should throw exception if the type of some required field ir wrong. Ex: nCdFormato as  string', function() {
+    (function() {
+      var args = {
+        nCdServico: '40010,41106,40215',
+        sCepOrigem: '31275000',
+        sCepDestino:'32400000',
+        nVlPeso :'2.300',
+        nCdFormato :'3',
+        nVlComprimento : 30.0,
+        nVlAltura : 3,
+        nVlLargura : 30.0,
+        nVlDiametro :30.0,
+        zeta:123
+      }
+      correios.calcPrecoPrazo(args);
+    }).should.throw();
   });
 });
